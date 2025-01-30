8be2217550605bf1687780a572796567261059e0
Manoj Manchikatla <123497531+Man7179@users.noreply.github.com>
2025-01-30 21:43:03 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,4 +1,3 @@
-// create token and saving that in cookies
 const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
 
@@ -16,5 +15,4 @@ const sendToken = (user, statusCode, res) => {
     token,
   });
 };
-
 module.exports = sendToken;
